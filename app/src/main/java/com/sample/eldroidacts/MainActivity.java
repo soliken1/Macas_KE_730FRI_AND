b5e54e3fedd6eb3dd50995c46c32156c579af9b8
Soliken <kennethrex456@gmail.com>
2024-10-01 12:53:41 +0800

    Commit For Bottom Nav

--- app/src/main/java/com/sample/eldroidacts/MainActivity.java
+++ app/src/main/java/com/sample/eldroidacts/MainActivity.java
@@ -1,8 +1,10 @@
 package com.sample.eldroidacts;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+        // Set default fragment
+        loadFragment(new CalculatorFragment());
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_calculator:
+                    fragment = new CalculatorFragment();
+                    break;
+                case R.id.navigation_list:
+                    fragment = new ListFragment();
+                    break;
+                case R.id.navigation_profile:
+                    fragment = new ProfileFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
+
 
